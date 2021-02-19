45eae9caa6f89aa051df0a0e0b2ba4fd10879e89
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-02-19 10:50:53 +0100

    Test

--- app/src/main/java/com/example/draw_and_pass/MainActivity.java
+++ app/src/main/java/com/example/draw_and_pass/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.draw_and_pass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void gotoOtherActivity(){
+        Intent intent = new Intent(this,OtherActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
