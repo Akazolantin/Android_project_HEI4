f8c5b979f0eeced127b9d85fedf77529cb4e7144
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-02-19 11:11:25 +0100

    Test

--- app/src/main/java/com/example/draw_and_pass/MainActivity.java
+++ app/src/main/java/com/example/draw_and_pass/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG="MainActivity";
 
     public void gotoOtherActivity(){
         Intent intent = new Intent(this,OtherActivity.class);
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.bouton);
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"click");
+                gotoOtherActivity();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/draw_and_pass/OtherActivity.java
+++ app/src/main/java/com/example/draw_and_pass/OtherActivity.java
@@ -2,13 +2,31 @@ package com.example.draw_and_pass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class OtherActivity extends AppCompatActivity {
+    private final String TAG="MainActivity";
+
+    public void gotoMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        Button button = (Button) findViewById(R.id.bouton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"click");
+                gotoMainActivity();
+            }
+        });
     }
 }
