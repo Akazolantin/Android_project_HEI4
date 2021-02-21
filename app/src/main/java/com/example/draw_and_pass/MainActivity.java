7e21f2c48f712893645319d72fac45d8e40a101c
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-02-21 12:15:52 +0100

    Dessin

--- app/src/main/java/com/example/draw_and_pass/MainActivity.java
+++ app/src/main/java/com/example/draw_and_pass/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.draw_and_pass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private final String TAG="MainActivity";
 
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.bouton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,4 +39,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/draw_and_pass/OtherActivity.java
+++ app/src/main/java/com/example/draw_and_pass/OtherActivity.java
@@ -2,14 +2,23 @@ package com.example.draw_and_pass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class OtherActivity extends AppCompatActivity {
-    private final String TAG="MainActivity";
+public class OtherActivity extends Activity {
+    private final String TAG="OtherActivity";
 
     public void gotoMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
@@ -21,6 +30,9 @@ public class OtherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
         Button button = (Button) findViewById(R.id.bouton);
+        DrawLineCanvas canvas = findViewById(R.id.canvas);
+        ImageButton pinceau = (ImageButton) findViewById(R.id.pinceau);
+        ImageButton gomme = (ImageButton) findViewById(R.id.gomme);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,5 +40,20 @@ public class OtherActivity extends AppCompatActivity {
                 gotoMainActivity();
             }
         });
+        pinceau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                canvas.setColor(0xff000000);
+                canvas.setSize(12);
+            }
+        });
+        gomme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                canvas.setColor(0xffffffff);
+                canvas.setSize(75);
+            }
+        });
     }
+
 }
