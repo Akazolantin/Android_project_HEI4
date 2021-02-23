d1b93c7a5d638f7489eeda7c24fc9cf2c8309edc
jeremy_BEAUROY-EUSTACHE <jeremybeauroyeustache@gmail.com>
2021-02-23 11:39:45 +0100

    ajout Règles.

--- app/src/main/java/com/example/draw_and_pass/Rules.java
+++ app/src/main/java/com/example/draw_and_pass/Rules.java
@@ -8,11 +8,13 @@ import android.widget.TextView;
 public class Rules extends AppCompatActivity {
 
     private TextView mTest_rules;
+    private TextView mTest_rules_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
         mTest_rules=(TextView)findViewById(R.id.activity_Rules_textView);
+        mTest_rules_title=(TextView)findViewById(R.id.activity_Rules_textView_Title);
     }
 }
