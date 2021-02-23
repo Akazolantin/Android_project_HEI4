259602ffaa08d96ccc0b1bea74d738e5462f6035
Antoine Benatoui <antoine.benatoui@hei.yncrea.fr>
2021-02-23 12:16:06 +0100

    gestion avatars

---
+++ app/src/main/java/com/example/draw_and_pass/Choix_Avatar.java
@@ -0,0 +1,14 @@
+package com.example.draw_and_pass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Choix_Avatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_avatar);
+    }
+}
