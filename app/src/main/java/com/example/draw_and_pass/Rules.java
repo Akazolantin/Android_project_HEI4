78eb7c586906a2e5d13cc3ba803bc8c5ddb6b2b6
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-02-23 18:26:06 +0100

    Rules

---
+++ app/src/main/java/com/example/draw_and_pass/Rules.java
@@ -0,0 +1,40 @@
+package com.example.draw_and_pass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Rules extends AppCompatActivity {
+
+    private TextView mTest_rules;
+    private TextView mTest_rules_title;
+    private Button mRules_Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        mTest_rules=(TextView)findViewById(R.id.activity_Rules_textView);
+        mTest_rules_title=(TextView)findViewById(R.id.activity_Rules_textView_Title);
+        mRules_Button=(Button)findViewById(R.id.activity_rules_button);
+
+
+
+
+        mRules_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Mainactivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(Mainactivity);
+                finish();
+
+            }
+        });
+    }
+
+
+}
