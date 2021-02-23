0921365d56502a1c8f9c9d20b88805f0336e51da
Thomas.Cappe <Azerty62280>
2021-02-23 08:31:33 +0100

    Modifs

--- app/src/main/java/com/example/draw_and_pass/MainActivity.java
+++ app/src/main/java/com/example/draw_and_pass/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     public void gotoOtherActivity(){
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //gotoEnd_gameActivity();
 
     }
