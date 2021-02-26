8ff3834d1299f452b8241393fd8f01a4a3f619cc
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-02-26 09:05:05 +0100

    Home page

--- app/src/main/java/com/example/draw_and_pass/MainActivity.java
+++ app/src/main/java/com/example/draw_and_pass/MainActivity.java
@@ -7,31 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 public class MainActivity extends Activity {
 
     int nbr_player;
     private TextView mApplication_Name;
-    private EditText mNumber_of_player;
+    private TextView mNumber_of_player;
     private Button mButton_to_rules;
+
     private Button mButton_to_play;
-    private static Game game;
-    private String Tag = "MainActivity";
     private Button mButton_to_increment;
     private Button mButton_to_decrement;
+    private static Game game;
 
 
     public void gotoOtherActivity(){
         Intent intent = new Intent(this,Transition.class);
-        Game game = new Game(0,nbr_player,new ArrayList<Event>(),null);// prendre les arguments sauvegardés dans le start game
-        game.addEvent(new Event(new User(0,"toto",null),"Thomas"));
+        Game game = new Game(0,nbr_player,null,null);// prendre les arguments sauvegardés dans le start game
         Transition.setGame(game);
         startActivity(intent);
     }
@@ -40,6 +36,11 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this,End_game.class);
         startActivity(intent);
     }
+    public void goToRulesActivity(){
+        Intent intent = new Intent(this,Rules.class);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +48,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_start_game);
 
         mApplication_Name = (TextView)findViewById(R.id.activity_Application_Name);
-        mNumber_of_player=(EditText)findViewById(R.id.activity_start_nombre_participants);
+        mNumber_of_player=(TextView) findViewById(R.id.activity_start_nombre_participants);
         mButton_to_rules=(Button)findViewById(R.id.activity_rules_button);
-
-        mButton_to_play=(Button)findViewById(R.id.activity_play_button);
 
         mButton_to_play=(Button)findViewById(R.id.activity_play_button);
         mButton_to_increment=(Button)findViewById(R.id.activity_increment_button);
@@ -74,28 +73,6 @@ public class MainActivity extends Activity {
             }
         });
 
-        mNumber_of_player.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mButton_to_play.setEnabled(s.toString().length() != 0);
-                try{
-                nbr_player = (int) Integer.parseInt(mNumber_of_player.getText().toString());}
-                catch(NumberFormatException e){
-                    Log.d(Tag,"erreur");
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
         mButton_to_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,12 +82,12 @@ public class MainActivity extends Activity {
         mButton_to_rules.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent RulesActivity = new Intent(getApplicationContext(),Rules.class);
-                startActivity(RulesActivity);
-
+                goToRulesActivity();
             }
         });
 
 
+
     }
 }
+
