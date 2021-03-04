fc0e49c467ab8efcdca92387bdaadf1f61934044
Antoine Benatoui <antoine.benatoui@hei.yncrea.fr>
2021-03-04 23:24:08 +0100

    ajout utilisation camera (pas tout juste ouverture cam)

--- app/src/main/java/com/example/draw_and_pass/Transition.java
+++ app/src/main/java/com/example/draw_and_pass/Transition.java
@@ -41,6 +41,7 @@ import java.util.Date;
 
 
 public class Transition extends Activity {
+    private static final int PHOTO_RESULT = 0;
     private EditText mPseudo;
     private Button mNextButton;
     private Button mButtonUp;
@@ -52,95 +53,23 @@ public class Transition extends Activity {
    private int positionAvatar;
    private TextView mTextADeviner;
    private ImageView mImageADeviner;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    private ImageView photo;
 
+    private File mFichier;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            photo.setImageBitmap(imageBitmap);
-        }
-    }
-    String currentPhotoPath;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == PHOTO_RESULT && resultCode== RESULT_OK){
+            if (data != null){
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                Bitmap photoUser = BitmapFactory.decodeFile("mFichier");
             }
         }
+
     }
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
-
-    private void setPic() {
-        // Get the dimensions of the View
-
-        int targetW = photo.getWidth();
-        int targetH = photo.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        photo.setImageBitmap(bitmap);
-    }
-
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
     private boolean back_answer = false;
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -214,6 +143,12 @@ public class Transition extends Activity {
             avatars.add(R.drawable.photocamera);
 
             mImageProfil.setImageResource(avatars.get(positionAvatar));
+
+
+
+
+
+
             mButtonUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -245,18 +180,32 @@ public class Transition extends Activity {
             });
 
 
-            if (positionAvatar==9){
-                mImageProfil.setOnClickListener(new View.OnClickListener(){
 
-                    @Override
-                    public void onClick(View v) {
-                        dispatchTakePictureIntent();
 
-                        galleryAddPic();
 
-                    }
-                });
-            }
+                    mImageProfil.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            if(positionAvatar==9) {
+                                System.out.println("position" + positionAvatar);
+                                mFichier = new File(Environment.getExternalStorageDirectory(), "photo.png");
+                                Uri fileUri ;
+                                fileUri = FileProvider.getUriForFile(
+                                        Transition.this,
+                                        "com.example.draw_and_pass.fileprovider",
+                                        mFichier);
+                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                                startActivityForResult(intent, PHOTO_RESULT);
+                            }
+
+
+                        }
+
+                    });
+
 
 
             if (game.getEvents().get(game.getEvents().size()-1).getImage() !=null){
