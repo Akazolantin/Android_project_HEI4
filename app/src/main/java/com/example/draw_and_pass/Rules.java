92647cda7796ef795e12d0dacc06dc815115405a
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-03-09 10:23:27 +0100

    Couleurs Rules

--- app/src/main/java/com/example/draw_and_pass/Rules.java
+++ app/src/main/java/com/example/draw_and_pass/Rules.java
@@ -2,13 +2,14 @@ package com.example.draw_and_pass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Rules extends AppCompatActivity {
+public class Rules extends Activity {
 
     private TextView mTest_rules;
     private TextView mTest_rules_title;
